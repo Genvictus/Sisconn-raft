48780e1da9c5909c3f8581b27a01e04f21687f9d
DewanaGustavus <76590469+DewanaGustavus@users.noreply.github.com>
2024-06-02 00:17:56 +0700

    style: client web logs scroll to bottom when update

--- client-web/src/components/Log.tsx
+++ client-web/src/components/Log.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 
 type LogProps = {
     logs: string;
@@ -6,10 +7,23 @@ type LogProps = {
 export default function Log({
     logs
 }: LogProps) {
+    const textAreaRef = useRef<HTMLTextAreaElement | null>(null);
+
+    useEffect(() => {
+        const textArea = textAreaRef.current;
+        if (textArea) {
+            textArea.scrollTop = textArea.scrollHeight;
+        }
+    }, [logs]);
+
     return (
         <div className="h-full w-full flex justify-center items-center p-4">
             <div className="h-full w-full border border-gray-300 rounded-md overflow-hidden">
-                <textarea className="w-full h-full p-4 resize-none overflow-auto outline-none border-none" value={logs} readOnly>
+                <textarea
+                    className="w-full h-full p-4 resize-none overflow-auto outline-none border-none"
+                    value={logs}
+                    ref={textAreaRef}
+                    readOnly >
                 </textarea>
             </div>
         </div>
