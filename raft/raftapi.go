8774764290e2a5b10cdc8bc45173e052ad57057b
Genvictus <johannstarkmk42@gmail.com>
2024-06-07 14:24:19 +0700

    add: raft AppendEntries implementation (receiver side)

--- raft/raftapi.go
+++ raft/raftapi.go
@@ -157,6 +157,53 @@ func (s *RaftServer) RequestVote(ctx context.Context, in *pb.RequestVoteArg) (*p
 	return &pb.VoteResult{VoteGranted: false}, nil
 }
 
-// func (s *RaftServer) AppendEntries(ctx context.Context, in *pb.AppendEntriesArg) (*pb.AppendResult, error) {
+func (s *RaftServer) AppendEntries(ctx context.Context, in *pb.AppendEntriesArg) (*pb.AppendResult, error) {
+	currentTerm := s.Server.currentTerm
+	res := pb.AppendResult{
+		Term: currentTerm,
+	}
+	// 1. Reply false if term < currentTerm (§5.1)
+	if in.Term < currentTerm {
+		res.Success = false
+		return &res, nil
+	}
+	// 2. Reply false if log doesn’t contain an entry at prevLogIndex
+	// whose term matches prevLogTerm (§5.3)
+	if s.Server.log.lastIndex < in.PrevLogIndex {
+		// if index is out of bound (newer entries)
+		res.Success = false
+		return &res, nil
+	}
+	entryTerm := s.Server.log.getEntries(in.PrevLogIndex, in.PrevLogIndex)[0].term
+	if entryTerm != in.PrevLogTerm {
+		res.Success = false
+		return &res, nil
+	}
 
-// }
+	// 3. If an existing entry conflicts with a new one (same index
+	// but different terms), delete the existing entry and all that
+	// follow it (§5.3)
+	// 4. Append any new entries not already in the log
+	logEntries := make([]keyValueReplicationEntry, len(in.LogEntries))
+	for index, entry := range in.LogEntries {
+		logEntries[index] = keyValueReplicationEntry{
+			term:  entry.GetTerm(),
+			key:   entry.GetKey(),
+			value: entry.GetValue(),
+		}
+	}
+	// start with goroutine? is this long running?
+	s.Server.log.replaceLog(in.PrevLogIndex+1, logEntries)
+
+	// 5. If leaderCommit > commitIndex, set commitIndex =
+	// min(leaderCommit, index of last new entry)
+	s.Server.log.indexLock.RLock()
+	commitIndex := s.Server.log.commitIndex
+	lastIndex := s.Server.log.lastIndex
+	s.Server.log.indexLock.RUnlock()
+	if in.LeaderCommit > commitIndex {
+		go s.Server.log.commitEntries(min(in.LeaderCommit, lastIndex))
+	}
+
+	return &res, nil
+}
