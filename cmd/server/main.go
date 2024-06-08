ad7c6546f8c06ee007720eb865f78fb1a3249e30
yansans <6671259+yansans@users.noreply.github.com>
2024-06-08 14:18:49 +0700

    feat: godot for config

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,6 +9,7 @@ import (
 	"net"
 	"os"
 
+	"github.com/joho/godotenv"
 	"google.golang.org/grpc"
 )
 
@@ -23,14 +24,21 @@ var ServerVerboseLogger *log.Logger
 var RaftNode *raft.RaftNode
 
 func main() {
+	ServerLogger = log.New(os.Stdout, "[Raft] Server: ", 0)
+
+	err := godotenv.Load()
+	if err != nil {
+		ServerLogger.Println("Error loading .env file. Using default values.")
+	} else {
+		raft.LoadRaftConfig()
+	}
+
 	var serverInfo ServerParserInfo
 	flag.StringVar(&serverInfo.Host, "host", "localhost", "Server hostname that will be used by client, default=localhost")
 	flag.IntVar(&serverInfo.Port, "port", 6969, "Server port that will be used by client, default=6969")
 	flag.Parse()
 
 	serverAddress := transport.NewAddress(serverInfo.Host, serverInfo.Port)
-
-	ServerLogger = log.New(os.Stdout, "[Raft] Server: ", 0)
 
 	transport.LogPrint(ServerLogger, "Server started")
 	// fmt.Println("Server started at", &serverAddress)
--- raft/config.go
+++ raft/config.go
@@ -1,6 +1,10 @@
 package raft
 
-import "time"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 // TIMEOUTS
 var CLIENT_TIMEOUT time.Duration = 500 * time.Millisecond
@@ -26,6 +30,60 @@ func SetRaftIntervals(heartbeat time.Duration, election_min time.Duration, elect
 	HEARTBEAT_INTERVAL = heartbeat
 	ELECTION_TIMEOUT_MIN = election_min
 	ELECTION_TIMEOUT_MAX = election_max
+}
+
+func LoadRaftConfig() {
+	// from os.env
+	timeoutClientStr := os.Getenv("CLIENT_TIMEOUT") + "ms"
+	timeoutClient, err := time.ParseDuration(timeoutClientStr)
+	if err != nil {
+		// Use default value
+		timeoutClient = CLIENT_TIMEOUT
+	}
+
+	timeoutServerStr := os.Getenv("SERVER_RPC_TIMEOUT") + "ms"
+	timeoutServer, err := time.ParseDuration(timeoutServerStr)
+	if err != nil {
+		// Use default value
+		// log.Println(err)
+		timeoutServer = SERVER_RPC_TIMEOUT
+	}
+
+	heartbeatStr := os.Getenv("HEARTBEAT_INTERVAL") + "ms"
+	heartbeat, err := time.ParseDuration(heartbeatStr)
+	if err != nil {
+		// Use default value
+		heartbeat = HEARTBEAT_INTERVAL
+	}
+
+	electionMinStr := os.Getenv("ELECTION_TIMEOUT_MIN") + "ms"
+
+	electionMin, err := time.ParseDuration(electionMinStr)
+	if err != nil {
+		// Use default value
+		electionMin = ELECTION_TIMEOUT_MIN
+	}
+
+	electionMaxStr := os.Getenv("ELECTION_TIMEOUT_MAX") + "ms"
+	electionMax, err := time.ParseDuration(electionMaxStr)
+	if err != nil {
+		// Use default value
+		electionMax = ELECTION_TIMEOUT_MAX
+	}
+
+	SetClientTimeout(timeoutClient)
+	SetServerRPCTimeout(timeoutServer)
+	SetRaftIntervals(heartbeat, electionMin, electionMax)
+
+	// PrintConfig()
+}
+
+func PrintConfig() {
+	fmt.Println("Client Timeout:", CLIENT_TIMEOUT)
+	fmt.Println("Server RPC Timeout:", SERVER_RPC_TIMEOUT)
+	fmt.Println("Heartbeat Interval:", HEARTBEAT_INTERVAL)
+	fmt.Println("Election Timeout Min:", ELECTION_TIMEOUT_MIN)
+	fmt.Println("Election Timeout Max:", ELECTION_TIMEOUT_MAX)
 }
 
 // Raft Constants
